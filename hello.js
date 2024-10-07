dee82e3d45753cdcb67ce5837fc6f16e85206462
EshanDev27 <laleshan7@gmail.com>
2024-10-07 09:40:01 +0530

    First commit

---
+++ hello.js
@@ -0,0 +1,21 @@
+var http = require("http");
+
+// Define the request listener function
+var listener = function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/html
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+   
+    // Send the response body as "Hello World"
+    response.end('<h2 style="text-align: center;">Hello World</h2>');
+};
+
+// Create the server and pass in the listener
+var server = http.createServer(listener);
+
+// Make the server listen on port 3000
+server.listen(3000);
+
+// Log the server status
+console.log('Server running at http://127.0.0.1:3000/');
